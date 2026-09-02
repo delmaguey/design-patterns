145a854450967fd480d32a00f7a5656fafea6f8b
Del Maguey Technologies <dan13lpj@hotmail.com>
2026-09-02 15:38:54 -0600

    Added Creational Builder Pattern

--- design-patterns/Program.cs
+++ design-patterns/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Patterns.Creational;
+
+
+var director = new Director();
+var builder = new ConcreteBuilder();
+
+director.Builder = builder;
+
+Console.WriteLine("Standard basic product:");
+director.BuildMinimalViableProduct();
+Console.WriteLine(builder.GetProduct().ListParts());
+
+Console.WriteLine("Standard full featured product:");
+director.BuildFullFeaturedProduct();
+Console.WriteLine(builder.GetProduct().ListParts());
+
+// Using builder pattern without a director
+Console.WriteLine("Custom product:");
+builder.BuildPartA();
+builder.BuildPartC();
+Console.WriteLine(builder.GetProduct().ListParts());
+
+
+
---
+++ design-patterns/src/CreationalPatterns/BuilderPattern.cs
@@ -0,0 +1,102 @@
+namespace Patterns.Creational
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+
+    public class ConcreteBuilder: IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+
+        public void BuildPartA()
+        {
+            this._product.Add("PartA1");
+        }
+
+        public void BuildPartB()
+        {
+            this._product.Add("PartB1");
+        }
+
+        public void BuildPartC()
+        {
+            this._product.Add("PartC1");
+        }
+
+        public Product GetProduct()
+        {
+            Product result = this._product;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+
+
+
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+    
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for(int i=0;i<this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length -2); // removing last comma and space
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+
+
+    public class Director
+        {
+            private IBuilder _builder;
+            
+            public IBuilder Builder
+            {
+                set { _builder = value; } 
+            }
+            
+            // The Director can construct several product variations using the same building steps.
+            public void BuildMinimalViableProduct()
+            {
+                this._builder.BuildPartA();
+            }
+            
+            public void BuildFullFeaturedProduct()
+            {
+                this._builder.BuildPartA();
+                this._builder.BuildPartB();
+                this._builder.BuildPartC();
+            }
+        }
+
+
+
+}
